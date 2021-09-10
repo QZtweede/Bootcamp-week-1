e5232e35d772edf57e61c90be3f6d618b556d8f0
Rovers,Zjuul Z <z.rovers@student.fontys.nl>
2021-09-10 13:51:08 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var s1
 var speed = 40
 var array = []
 var val = 0
+var countS2 = 1
    
 async function startIntro(){
     startCircle.style.transition = 1 + "s"
@@ -13,6 +14,19 @@ async function startIntro(){
     await sleep(1500)
     section1()
     section2()
+    await sleep(150)
+    section2()
+    await sleep(150)
+    section2()
+    await sleep(250)
+    section2()
+    await sleep(150)
+    section2()
+    await sleep(150)
+    section2()
+    await sleep(1000)
+    destroy()
+    countS2 = 1
 }
 
 async function section1(){
@@ -30,75 +44,87 @@ async function section1(){
     s1.style.opacity = 0
     createCircles()  
     s1.style.marginRight = 0 + "vw"
-    // destroy()
 }
 
 async function section2(){
+    if(countS2 >= 1.3){
+        countS2 = 1
+    }
     await sleep(200)
     createCircles()  
-    s1.style.marginRight = 22 + "vw"
     createCircles()  
-    s1.style.marginLeft = 22 + "vw"
+    s1.style.marginRight = 22 * countS2 + "vw"
     createCircles()  
-    s1.style.marginTop = 22 + "vw"
+    s1.style.marginLeft = 22 * countS2 + "vw"
     createCircles()  
-    s1.style.marginBottom = 22 + "vw"
+    s1.style.marginTop = 22 * countS2 + "vw"
     createCircles()  
-    s1.style.marginBottom = 22 + "vw"
-    s1.style.marginLeft = 22 + "vw"
+    s1.style.marginBottom = 22 * countS2 + "vw"
     createCircles()  
-    s1.style.marginTop = 22 + "vw"
-    s1.style.marginLeft = 22 + "vw"
+    s1.style.marginBottom = 22 * countS2 + "vw"
+    s1.style.marginLeft = 22 * countS2 + "vw"
     createCircles()  
-    s1.style.marginBottom = 22 + "vw"
-    s1.style.marginRight = 22 + "vw"
+    s1.style.marginTop = 22 * countS2 + "vw"
+    s1.style.marginLeft = 22 * countS2 + "vw"
     createCircles()  
-    s1.style.marginTop = 22 + "vw"
-    s1.style.marginRight = 22 + "vw"
+    s1.style.marginBottom = 22 * countS2 + "vw"
+    s1.style.marginRight = 22 * countS2 + "vw"
+    createCircles()  
+    s1.style.marginTop = 22 * countS2 + "vw"
+    s1.style.marginRight = 22 * countS2 + "vw"
     await sleep(speed)
     createCircles()  
-    s1.style.marginLeft = 44 + "vw"
+    s1.style.marginLeft = 44 * countS2 + "vw"
     createCircles()  
-    s1.style.marginLeft = 66 + "vw"
+    s1.style.marginLeft = 66 * countS2 + "vw"
     createCircles()  
-    s1.style.marginRight = 44 + "vw"
+    s1.style.marginRight = 44 * countS2 + "vw"
     createCircles()  
-    s1.style.marginRight = 66 + "vw"
+    s1.style.marginRight = 66 * countS2 + "vw"
     createCircles()  
-    s1.style.marginTop = 44 + "vw"
+    s1.style.marginTop = 44 * countS2 + "vw"
     createCircles()  
-    s1.style.marginTop = 66 + "vw"
+    s1.style.marginTop = 66 * countS2 + "vw"
     createCircles()  
-    s1.style.marginBottom = 44 + "vw"
+    s1.style.marginBottom = 44 * countS2 + "vw"
     createCircles()  
-    s1.style.marginBottom = 66 + "vw" 
+    s1.style.marginBottom = 66 * countS2 + "vw" 
     createCircles()  
-    s1.style.marginLeft = 38 + "vw"
-    s1.style.marginTop = 38 + "vw"
+    s1.style.marginLeft = 38 * countS2 + "vw"
+    s1.style.marginTop = 38 * countS2 + "vw"
     createCircles()  
-    s1.style.marginLeft = 54 + "vw"
-    s1.style.marginTop = 54 + "vw"
+    s1.style.marginLeft = 54 * countS2 + "vw"
+    s1.style.marginTop = 54 * countS2 + "vw"
     createCircles()  
-    s1.style.marginRight = 38 + "vw"
-    s1.style.marginTop = 38 + "vw"
+    s1.style.marginRight = 38 * countS2 + "vw"
+    s1.style.marginTop = 38 * countS2 + "vw"
     createCircles()  
-    s1.style.marginRight = 54 + "vw"
-    s1.style.marginTop = 54 + "vw"
+    s1.style.marginRight = 54 * countS2 + "vw"
+    s1.style.marginTop = 54 * countS2 + "vw"
     createCircles()  
-    s1.style.marginLeft = 38 + "vw"
-    s1.style.marginBottom = 38 + "vw"
+    s1.style.marginLeft = 38 * countS2 + "vw"
+    s1.style.marginBottom = 38 * countS2 + "vw"
     createCircles()  
-    s1.style.marginLeft = 54 + "vw"
-    s1.style.marginBottom = 54 + "vw"
+    s1.style.marginLeft = 54 * countS2 + "vw"
+    s1.style.marginBottom = 54 * countS2 + "vw"
     createCircles()  
-    s1.style.marginRight = 38 + "vw"
-    s1.style.marginBottom = 38 + "vw"
+    s1.style.marginRight = 38 * countS2 + "vw"
+    s1.style.marginBottom = 38 * countS2 + "vw"
     createCircles()  
-    s1.style.marginRight = 54 + "vw"
-    s1.style.marginBottom = 54 + "vw" 
+    s1.style.marginRight = 54 * countS2 + "vw"
+    s1.style.marginBottom = 54 * countS2 + "vw" 
+    countS2 = countS2 + 0.2
     await sleep(100)
-    for(var i = 0; i < 28; i++){
+    for(var i = 0; i < array.length; i++){
         document.getElementById("c" + i).style.backgroundColor = "#ff2f00"
+    }
+    await sleep(100)
+    for(var i = 0; i < array.length; i++){
+        document.getElementById("c" + i).style.backgroundColor = "#ff78a7"
+    }
+    await sleep(250)
+    for(var i = 0; i < array.length; i++){
+        document.getElementById("c" + i).style.opacity = 0
     }
 }
 
@@ -110,13 +136,14 @@ function createCircles(){
     s1.style.opacity = 1   
     array.push(s1)
     val++
+    s1.style.transform = "scale(" + countS2 + ")"
 }
 
-// function destroy(){
-//     for(var j; j < 10; j++){
-//         s1.remove()
-//     }
-// }
+function destroy(){
+    for(var i = 0; i < array.length; i++){
+        document.getElementById("c" + i).remove()
+    }
+}
 
 function sleep(time) {      //Functie om pauzes te maken
     return new Promise(resolve => setTimeout(resolve, time));
